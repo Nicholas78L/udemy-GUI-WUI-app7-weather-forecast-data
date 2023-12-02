ba82cce043e2ee25ea7fd4a065b31563280f8602
Nicholas78L <nikolja191278@gmail.com>
2023-12-02 23:24:02 +0200

    Day 32: Plot fake data dynamically + add plotly lab to requirements.txt

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def get_data(days):
     dates = ['2022-25-10', '2022-26-10', '2022-27-10']
     temperatures = [10, 11, 15]
     temperatures = [days * i for i in temperatures]
-    return temperatures
+    return dates, temperatures
 
 
 d, t = get_data(days)
