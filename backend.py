8a518a9f74375f57e8b878d4dc7cbb502c898298
Nicholas78L <nikolja191278@gmail.com>
2023-12-04 22:25:28 +0200

    Add images corresponding to the sky conditions.

--- backend.py
+++ backend.py
@@ -1,22 +1,20 @@
 import requests
 
-API_KEY = '71799afa63f51ded6d9d0396acf277f2'
+API_KEY = '71799afa63f51ded6d9d0396acf277f2'  # Ключ, полученный на сайте https://openweathermap.org/
 cityname = 'Tokyo'
 
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f'http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}'
     response = requests.get(url)
-    data = response.json()
-    filtered_data = data['list']
-    nr_values = 8 * forecast_days
-    filtered_data = filtered_data[:nr_values]
-    if kind == 'Temperature':
-        filtered_data = [dict['main']['temp'] for dict in filtered_data]
-    if kind == 'Sky':
-        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
-    return filtered_data
+    data = response.json()         # Получаем огромный массив несортированых json данных.
+    filtered_data = data['list']   # Выбираем только данные находящиеся в разделах 'list'
+    # Т.к. сайт даёт данные с периодичностью в 3 часа (за сутки 8 раз) определяем число выборок данных
+    nr_values = 8 * forecast_days  # исходя из произведения выборок за сутки на нужное кол-во дней (forecast_days)
+    filtered_data = filtered_data[:nr_values]  # Делаем срез отфильтрованых данных исходя из нужного кол-ва выборок.
+    return filtered_data          # Возвращщаем данные в место вызова ф-ции
 
 
+# Создаём конструкцию для проверки работоспособности backend-a без запуска файла main.py:
 if __name__ == '__main__':
-    print(get_data(place='Tokyo', forecast_days=3, kind='Temperature'))
+    print(get_data(place='Tokyo', forecast_days=3))
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+# Add title, text_input, slider, selectbox and subheader widgets:
 st.title('Weather Forecast for the Next Days')
 place = st.text_input('Place: ')
 days = st.slider('Forecast Days', min_value=1, max_value=5,
@@ -9,11 +10,23 @@ days = st.slider('Forecast Days', min_value=1, max_value=5,
 option = st.selectbox('Select data to view', ['Temperature', 'Sky'])
 st.subheader(f'{option} for the next {days} day(s) in {place}')
 
-data = get_data(place, days, option)
+if place:   # Дальнейший код будет выполняться только после присвоения значения переменной 'place'.
+    # Get temperature/sky data:
+    filtered_data = get_data(place, days)
 
+    if option == 'Temperature':  # Делаем выборку (для возврата данных в виде списка) по Температуре:
+        temperatures = [dict['main']['temp'] for dict in filtered_data]  # Выборки по температурам.
+        dates = [dict['dt_txt'] for dict in filtered_data]               # Выборки по датам.
+        # Создаём график изменения Температуры:
+        figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
+        st.plotly_chart(figure)
 
-d, t = get_data(days)
-
-# Создаём фигуру будущего графика:
-figure = px.line(x=d, y=t, labels={'x': 'Date', 'y': 'Temperature (C)'})
-st.plotly_chart(figure)
+    if option == 'Sky':  # Делаем выборку (для возврата данных в виде списка) по Небу:
+        images = {'Clear': 'images/clear.png', 'Clouds': 'images/cloud.png',
+                  'Rain': 'images/rain.png', 'Snow': 'images/snow.png'}
+        sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+        # Делаем "ПЕРЕВОД" данных, из строкового формата в рисунок. Для этого ключи словаря 'images'
+        # делаем идентичными значениям, хранящимся в списке 'sky_conditions'. А значениями этих ключей
+        # выступают пути хранения соответствующих рисунков:
+        image_paths = [images[condition] for condition in sky_conditions]
+        st.image(image_paths, width=115)
