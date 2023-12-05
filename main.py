0625ab19acc581b2c989aa783ceaac8cdc4aca2d
Nicholas78L <nikolja191278@gmail.com>
2023-12-05 12:58:51 +0200

    Added a try-except construct in case of misspelling the name of a place.

--- main.py
+++ main.py
@@ -11,22 +11,26 @@ option = st.selectbox('Select data to view', ['Temperature', 'Sky'])
 st.subheader(f'{option} for the next {days} day(s) in {place}')
 
 if place:   # Дальнейший код будет выполняться только после присвоения значения переменной 'place'.
-    # Get temperature/sky data:
-    filtered_data = get_data(place, days)
+    try:
+        # Get temperature/sky data:
+        filtered_data = get_data(place, days)
 
-    if option == 'Temperature':  # Делаем выборку (для возврата данных в виде списка) по Температуре:
-        temperatures = [dict['main']['temp'] for dict in filtered_data]  # Выборки по температурам.
-        dates = [dict['dt_txt'] for dict in filtered_data]               # Выборки по датам.
-        # Создаём график изменения Температуры:
-        figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
-        st.plotly_chart(figure)
+        if option == 'Temperature':  # Делаем выборку (для возврата данных в виде списка) по Температуре:
+            temperatures = [(dict['main']['temp']/10-32)*5/9 for dict in filtered_data]  # Выборки по температурам.
+            dates = [dict['dt_txt'] for dict in filtered_data]               # Выборки по датам.
+            # Создаём график изменения Температуры:
+            figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
+            st.plotly_chart(figure)
 
-    if option == 'Sky':  # Делаем выборку (для возврата данных в виде списка) по Небу:
-        images = {'Clear': 'images/clear.png', 'Clouds': 'images/cloud.png',
-                  'Rain': 'images/rain.png', 'Snow': 'images/snow.png'}
-        sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
-        # Делаем "ПЕРЕВОД" данных, из строкового формата в рисунок. Для этого ключи словаря 'images'
-        # делаем идентичными значениям, хранящимся в списке 'sky_conditions'. А значениями этих ключей
-        # выступают пути хранения соответствующих рисунков:
-        image_paths = [images[condition] for condition in sky_conditions]
-        st.image(image_paths, width=115)
+        if option == 'Sky':  # Делаем выборку (для возврата данных в виде списка) по Небу:
+            images = {'Clear': 'images/clear.png', 'Clouds': 'images/cloud.png',
+                      'Rain': 'images/rain.png', 'Snow': 'images/snow.png'}
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            # Делаем "ПЕРЕВОД" данных, из строкового формата в рисунок. Для этого ключи словаря 'images'
+            # делаем идентичными значениям, хранящимся в списке 'sky_conditions'. А значениями этих ключей
+            # выступают пути хранения соответствующих рисунков:
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+
+    except KeyError:
+        st.info('Oh, you entered a non-existing place.')
